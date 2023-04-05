593cffb63b648d4f254957093797a9ceb0e48718
Keerthi-Korrapati <103488773+Keerthi-Korrapati@users.noreply.github.com>
2023-04-05 15:23:45 +0530

    Update Demo.java

--- Maven_project/src/main/java/com/capg/jenkins/Demo.java
+++ Maven_project/src/main/java/com/capg/jenkins/Demo.java
@@ -1,15 +1,21 @@
 package com.capg.jenkins;
 
 public class Demo {
-	public static void main(String args[]){
-	
+	public static void main(String[] args) {
+		
+		int result =addNumbers(5,5);
+		System.out.println(result);
+		
+		int result1 =substractNumbers(5,5);
+		System.out.println(result1);
+		
+	}
 	public int addNumbers(int n1,int n2) {
 		return n1+n2;
 	}
 	
 	public int substractNumbers(int n1,int n2) {
 		return n1-n2;
-	}
 	}
 
 }
